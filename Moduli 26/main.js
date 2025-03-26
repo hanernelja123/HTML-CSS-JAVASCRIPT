30bb7b794c8e3d8b199391f4be31e3b95fcfb962
hanernelja123 <haner7852@gmail.com>
2025-03-26 12:44:39 -0700

    Moduli 26

---
+++ Moduli 26/main.js
@@ -0,0 +1,27 @@
+console.log("hello");
+
+var a = 50 + 50;
+console.log(a);
+
+var b = 70;
+var c = 90;
+var d = (b+c);
+console.log(d);
+
+var e = 20;
+var f = 10;
+var g = (e-f);
+console.log(g);
+
+var h = 100;
+var i = 20;
+var rezultati = h/i;
+console.log(rezultati);
+
+var n=100;
+n++
+console.log(n);
+
+var o=100;
+o--;
+console.log(o);
